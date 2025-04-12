3295d63d0c0918e627c24088c435c315d4480e84
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-04-12 18:46:11 -0300

    mapeamento JPA da entidade Autor + uso do lombock (getter e setter)

---
+++ src/main/java/io/github/lorimedeiros/livraria_api/model/Autor.java
@@ -0,0 +1,30 @@
+package io.github.lorimedeiros.livraria_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Autor") //aqui é posto o nome da tabela como está no db (arquivo "comandos-criacao-tabelas.txt")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50)
+    private String nacionalidade;
+
+}
